6f07f60a3bbe51ac3a81f40650ac1134defedc35
Suhani Agarwal <162464433+5uhaniagarwal@users.noreply.github.com>
2024-03-09 19:38:42 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,9 +1,35 @@
-//create webserver
-var http = require('http'); 
-var fs = require('fs');
-var url = require('url');
-var path = require('path');
-var comments = require('./comments');
-var mime = require('mime');
-var cache = {};
+//create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/mean-demo', { useNewUrlParser: true });
+
+var Comment = mongoose.model('Comment', {
+  title: String,
+  content: String,
+  date: Date
+});
+
+app.get('/api/comments', function (req, res) {
+  Comment.find(function (err, comments) {
+    if (err) return console.error(err);
+    res.json(comments);
+  });
+});
+
+app.post('/api/comments', function (req, res) {
+  var comment = new Comment(req.body);
+  comment.save(function (err, comment) {
+    if (err) return console.error(err);
+    res.json(comment);
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Server is running on http://localhost:3000');
+});
 
