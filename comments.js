f9669a5a4fdd6a6bea33449d74715eb5a5467f63
Suhani Agarwal <162464433+5uhaniagarwal@users.noreply.github.com>
2024-03-10 08:11:00 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,14 +1,49 @@
 //create web server
-const http = require('http');
-
-// Create a simple web server that responds with "Hello, World!" to all requests
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var ROOT_DIR = "html/";
+var comments = [];
+var server = http.createServer(function (req, res) {
+    var urlObj = url.parse(req.url, true, false);
+    console.log("URL path: " + urlObj.pathname);
+    console.log("URL search: " + urlObj.search);
+    console.log("URL query: " + urlObj.query["q"]);
+    // If this is our comments REST service
+    if(urlObj.pathname.indexOf("comment") !=-1) {
+        if(req.method === "POST") {
+            // Read the body data
+            var jsonData = "";
+            req.on('data', function (chunk) {
+                jsonData += chunk;
+            });
+            req.on('end', function () {
+                var reqObj = JSON.parse(jsonData);
+                console.log(reqObj);
+                console.log("Name: " + reqObj.Name);
+                console.log("Comment: " + reqObj.Comment);
+                comments.push(reqObj);
+                console.log(comments);
+                res.writeHead(200);
+                res.end("");
+            });
+        } else if(req.method === "GET") {
+            var commentsStr = JSON.stringify(comments);
+            res.writeHead(200);
+            res.end(commentsStr);
+        }
+    } else {
+        // Serve static files
+        fs.readFile(ROOT_DIR + urlObj.pathname, function (err, data) {
+            if (err) {
+                res.writeHead(404);
+                res.end(JSON.stringify(err));
+                return;
+            }
+            res.writeHead(200);
+            res.end(data);
+        });
+    }
 });
-
-// Start the server on port 3000
-server.listen(3000, '127.0.0.1', () => {
-  console.log('Server running at http://127.0.0.1:3000/');
-});
+server.listen(3000);
+console.log('Server is listening on port 3000');
